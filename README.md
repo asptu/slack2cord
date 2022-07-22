f8526b79173c5368feee3e8f19eac5723137893e
asptu <84566762+asptu@users.noreply.github.com>
2022-07-23 01:21:50 +1000

    slack attachment support

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ If all has been done correctly then it should be working, if not contact **Asptu
 ## Todo
 
 - Make a cache list for slack users profile pictures and names as getting them in the first place is quite a slow process
-- Support for slack attachments to discord?
+- ~~Support for slack attachments to discord?~~ ✅
 - Maybe redoing how slack to discord threads work as they're kinda weird
 
 Heavily inspired by https://github.com/HackSoc/slack-discord-bridge
--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { Client, Intents, WebhookClient } = require("discord.js");
 require("dotenv").config();
 
 const slack_web = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+
 const discord_client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -21,9 +23,7 @@ const slack_app = new App({
 });
 
 slack_app.event("message", async ({ message }) => {
-
   // Make an array of all the slack channel ids in your workspace and then an array with all of the webhooks links then Position 1 of slack array will match with Position 1 in discord array
-
   // Example:
   var slackChats = [
     'DJSJSDDS',
@@ -38,10 +38,7 @@ slack_app.event("message", async ({ message }) => {
   if (slackChats.indexOf(message.channel) > -1) {
     let n_discordchats = slackChats.lastIndexOf(message.channel);
 
-    if (message.attachments) if (message.type.bot) return;
-    if (message.text == null || undefined) return;
-    if (message.text == "") return;
-    if (message.text.includes("@everyone" || "@here")) return;
+    if (message.type.bot) return;
 
     const webhookClient = new WebhookClient({
       url: discordChats[n_discordchats],
@@ -50,21 +47,97 @@ slack_app.event("message", async ({ message }) => {
     let p = await slack_app.client.users.profile.get({ user: message.user });
 
     if (message.parent_user_id !== undefined) {
+      
       let pThread = await slack_app.client.users.profile.get({
         user: message.parent_user_id,
       });
-
+       if (message.text !== "") {
+       if (message.text == null || undefined) return;
+       if (message.text.includes("@everyone" || "@here")) return;
+      console.log('thread detected')
       webhookClient.send({
         content: `**Replying to ${pThread.profile.display_name} ⇒** ${message.text}`,
         username: `Slack: ${p.profile.display_name}`,
         avatarURL: p.profile.image_192,
       });
+    }
     } else {
+      if (message.text !== "") {
+      if (message.text == null || undefined) return;
+      if (message.text.includes("@everyone" || "@here")) return;
+      console.log('message detected')
       webhookClient.send({
         content: message.text,
         username: `Slack: ${p.profile.display_name}`,
         avatarURL: p.profile.image_192,
       });
+      }
+    }
+
+    if (message.files !== undefined) {
+      console.log("true");
+      for (let i of message.files) {
+        const fs = require("fs");
+        const request = require("request");
+
+        const download = (url, path, callback) => {
+          request.head(url, (err, res, body) => {
+            request(url).pipe(fs.createWriteStream(path)).on("close", callback);
+          });
+        };
+
+        const url = {
+          url: i.url_private_download,
+          headers: {
+            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+          },
+        };
+        const path = `${__dirname}/attachments/${i.id}.${i.filetype}`;
+
+        download(url, path, async () => {
+          console.log("Downloaded");
+          let stats = fs.statSync(path);
+          let fileSizeInBytes = stats.size;
+          let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+          console.log(fileSizeInMegabytes);
+
+          if (fileSizeInMegabytes > 8) return console.log("file too large");
+
+          if (message.parent_user_id !== undefined) {
+
+            let pThread = await slack_app.client.users.profile.get({
+              user: message.parent_user_id,
+            });
+          console.log('attachment thread detected')
+          await webhookClient.send({
+            content: `**Replying to ${pThread.profile.display_name} ⇒**`,
+            files: [
+              {
+                attachment: path,
+                name: `${i.id}.${i.filetype}`,
+              },
+            ],
+            username: `Slack: ${p.profile.display_name}`,
+            avatarURL: p.profile.image_192,
+          });
+
+        } else {
+          console.log('attachment message detected')
+          await webhookClient.send({
+            files: [
+              {
+                attachment: path,
+                name: `${i.id}.${i.filetype}`,
+              },
+            ],
+            username: `Slack: ${p.profile.display_name}`,
+            avatarURL: p.profile.image_192,
+          });
+        }
+
+          fs.unlinkSync(path);
+        });
+      }
     }
   }
 });
